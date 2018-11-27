b4d898bb18f7cf0a5b9b61753c59b3fc4e12c6f5
Sheaffy <sheaffy.ashley@gmail.com>
2018-11-27 14:38:53 +0000

    Initial commit
    This is the first commit with stable working code.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # node-red-contrib-monzo
-This is a wrapper that will allow a connection to the monzo bank API for use within node-red
+
+### This is a node-red package that will allow you to retrive information from monzo, using your access token.
+
+This is an easy to use node, all you have to do is input a access token and select what information you want under the request dropdown.
+
+
+
+- [Installation](#installation)
+- [Getting your AccessToken](#token)
+- [Useage](#useage)
+
+### <a name="installation"></a> Installation
+##### <a name="installation-npm"></a> Install via npm
+```js
+npm install node-red-contrib-monzo
+```
+
+### <a name="token"></a> Getting your Token
+##### <a name="token-npm"></a> Install via npm
+To get your access token you will need to go to "https://developers.monzo.com/api/playground" and log in with your monzo account, you will see your access token on the playground page.
---
+++ monzo/monzo.js
@@ -0,0 +1,101 @@
+module.exports = function(RED) {
+    function MonzoCredentialsNode(n) {
+        const Monzo = require('monzo-js');
+        RED.nodes.createNode(this, n);
+    }
+    RED.nodes.registerType("monzo-credentials", MonzoCredentialsNode, {
+        credentials: {
+            token: {
+                type: "password"
+            }
+        }
+    });
+
+    function MonzoNodeIn(config) {
+        RED.nodes.createNode(this, config);
+        this.requesttype = config.requesttype;
+        this.monzoConfig = RED.nodes.getNode(config.monzocreds);
+        const Monzo = require('monzo-js');
+        if (!this.monzoConfig) {
+            this.status({
+                fill: "red",
+                shape: "dot",
+                text: "no token"
+            });
+        } else {
+            this.status({
+                fill: "green",
+                shape: "dot",
+                text: "ready"
+            });
+        }
+        var node = this;
+        node.on('input', function(msg) {
+            if (!this.monzoConfig) {
+                this.status({
+                    fill: "red",
+                    shape: "dot",
+                    text: "no token"
+                });
+                node.error("you have not entered a token", msg);
+            } else {
+                const monzo = new Monzo(this.monzoConfig.credentials.token);
+                if (this.requesttype == "accounts") {
+                    monzo.accounts.all().then(accounts => {
+                        for (const [id, acc] of accounts) {
+                            msg.payload = {
+                                "response": acc._account
+                            };
+                            node.send(msg);
+                        }
+                    }).catch(error => {
+                        node.error("your token is not authenticated.", msg);
+                        this.status({
+                            fill: "red",
+                            shape: "dot",
+                            text: "no auth"
+                        });
+                    });
+                }
+                if (this.requesttype == "balances") {
+                    monzo.accounts.all().then(accounts => {
+                        for (const [id, acc] of accounts) {
+                            msg.payload = {
+                                "response": acc._balance._balance
+                            };
+                            node.send(msg);
+                        }
+                    }).catch(error => {
+                        node.error("your token is not authenticated.", msg);
+                        this.status({
+                            fill: "red",
+                            shape: "dot",
+                            text: "no auth"
+                        });
+                    });
+                }
+                if (this.requesttype == "pots") {
+                    monzo.pots.all().then(pots => {
+                        for (const [id, pot] of pots) {
+                            msg.payload = {
+                                "response": {
+                                    "pot_name": pot.name,
+                                    "pot_balance": pot.balance
+                                }
+                            };
+                            node.send(msg);
+                        }
+                    }).catch(error => {
+                        node.error("your token is not authenticated.", msg);
+                        this.status({
+                            fill: "red",
+                            shape: "dot",
+                            text: "no auth"
+                        });
+                    });
+                }
+            }
+        });
+    }
+    RED.nodes.registerType("monzo-in", MonzoNodeIn);
+}
