49e099f4e59d8469df149a14e1a15acd7958224f
Sheaffy <sheaffy.ashley@gmail.com>
2018-11-30 02:53:29 +0000

    Once Click Authentication
    With a one click authentication by entering your clientid and secret, then clicking authorise with monzo, it will take you through the authentication process. Which will in turn, get a refresh token, this will be used to keep permanent authentication

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ This is an easy to use node, all you have to do is input a access token and sele
 
 
 - [Installation](#installation)
-- [Getting your AccessToken](#token)
+- [Getting your temporary AccessToken](#temptoken)
+- [Getting your permanent AccessToken](#permtoken)
 - [Usage](#usage)
 - [Capability](#cap)
 
@@ -18,11 +19,14 @@ This is an easy to use node, all you have to do is input a access token and sele
 npm install node-red-contrib-monzo
 ```
 
-### <a name="token"></a> Getting your Token
+### <a name="temptoken"></a> Getting your temporary token
 To get your access token you will need to go to "https://developers.monzo.com/api/playground" and log in with your monzo account, you will see your access token on the playground page.
 
+### <a name="permtoken"></a> Getting your permanent token
+In order to have permanent authentication, this will require you to set up a monzo API client see "https://developers.monzo.com/apps/new", so that you can have a clientID and a API secret. (please node, the client will need Confidential set to TRUE, otherwise it will not automatically refresh).
+
 ### <a name="usage"></a> How to use this node.
-After you have placed the node, enter its configuration and add a token. Once added, select the request type you would like to do. To make a request simply inject anything into the input node to get a response from the output node.
+After you have placed the node, enter its configuration you can either add a temporary token or a clientID and API secret. If you added the clientID and secret you will now need to go through the authentication process by clicking "Authenticate With Monzo". Once a access token is added, select the request type you would like to do. To make a request simply inject anything into the input node to get a response from the output node.
 
 ### <a name="cap"></a> Capability
 This node can retreive three kinds of information from your monzo account.
--- monzo/monzo.js
+++ monzo/monzo.js
@@ -1,21 +1,196 @@
 module.exports = function(RED) {
+    "use strict";
+    var request = require("request");
+    var querystring = require("querystring");
+    var CronJob = require('cron').CronJob;
+
+
+    
+
+
     function MonzoCredentialsNode(n) {
-        const Monzo = require('monzo-js');
+       
         RED.nodes.createNode(this, n);
+      
+        this.cronjob = null;
+        var node = this;
+
+        this.cronjob = new CronJob('* */60 * * * *', function() {
+
+            const Monzo = require('monzo-js');
+            var creds = RED.nodes.getCredentials(node.id);
+            var secret = creds.secret;
+            var clientid = creds.client_id;
+            var refreshtoken = creds.refreshtoken;
+
+            if(refreshtoken != ""){
+
+                console.log("monzo - refreshing token");
+
+                Monzo.OAuth.refreshToken(clientid, secret, refreshtoken).then(({access_token, refresh_token}) => {
+                    
+                    if(access_token){
+                        console.log("monzo - refresh complete");
+                        var credentials = {
+                                client_id:clientid,
+                                secret:secret,
+                                token:access_token,
+                                refreshtoken:refresh_token
+                        };
+
+                        RED.nodes.addCredentials(node.id,credentials);
+                    }
+
+
+                }).catch(error => {
+                        console.log("monzo - refresh failed, needs reauthenticating");
+                });
+
+
+            } else {
+                //no refresh token dont bother trying to refresh it.
+            }
+
+
+        }, null, true, 'America/Los_Angeles');
+
+
     }
+
+
+    MonzoCredentialsNode.prototype.close = function() {
+            this.cronjob.stop();
+            delete this.cronjob;
+    };
+
+
     RED.nodes.registerType("monzo-credentials", MonzoCredentialsNode, {
         credentials: {
+            client_id: {
+                type: "text"
+            },
+            secret: {
+                type: "text"
+            },
             token: {
-                type: "password"
+                type: "text"
+            },
+            refreshtoken: {
+                type: "text"
             }
         }
     });
+
+
+   
+    RED.httpAdmin.get('/monzo-creds-set', function(req, res) {
+
+            var secret = req.query.secret;
+            var clientid = req.query.clientid;
+            var nodeid = req.query.nodeid;
+            if(secret != "" && clientid != "" && nodeid != ""){
+                var credentials = {
+                    client_id:clientid,
+                    secret:secret,
+                };
+
+                RED.nodes.addCredentials(nodeid, credentials);
+
+                res.send("success");
+            } else {
+                res.send("fail");
+            }
+
+    });
+
+    //set up endpoint for monzo to redirect back to.   
+    RED.httpAdmin.get('/monzo-creds', function(req, res) {
+            
+            //upon redirecting get the auth token and post to monzo to get the refresh token and the access_token
+            var credential_node_id = req.query.state;
+
+            var monzocreds = RED.nodes.getCredentials(credential_node_id);
+
+            var opts = {};
+            opts.url = "https://api.monzo.com/oauth2/token";
+            opts.timeout = 2000;
+            opts.method = "POST";
+            opts.headers = {};
+            opts.maxRedirects = 21;
+
+            var protocol = "";
+            if(req.connection.encrypted){
+                protocol = "https";
+            } else {
+                protocol = "http";
+            }
+
+            var auth_token = req.query.code;
+            var postvars = {
+                grant_type:"authorization_code",
+                client_id:monzocreds.client_id,
+                client_secret:monzocreds.secret,
+                redirect_uri:protocol+"://"+req.get('host')+"/monzo-creds",
+                code:auth_token
+            }
+            opts.form = postvars;
+
+            request(opts, function(err, ress, body) {
+                if(err){
+                    //console.log(err)
+                    res.send(err);
+     
+                }else{
+                    //console.log(body);
+                    var bodyObject = JSON.parse(body);
+                    //var monzocredentialConfig = RED.nodes.getNode(credential_node_id);
+                    //monzocredentialConfig.credentials.token = bodyObject.access_token;
+                    var additional_text = "";
+                    if(bodyObject.refresh_token){
+                        
+                        additional_text = "<br>This API client has Confidential set to True, because of this, your access_token will automatically refresh itself.";
+
+                    } else {
+                        console.log('no refresh token');
+                        additional_text = "<br>This token will expire and you will need to manually refresh it. This is because your Monzo API client is set to (Not Confidential).";
+                    }
+
+                    if(bodyObject.access_token){
+                        var credentials = {
+                            client_id:monzocreds.client_id,
+                            secret:monzocreds.secret,
+                            token:bodyObject.access_token,
+                            refreshtoken:bodyObject.refresh_token
+                        };
+
+                        RED.nodes.addCredentials(credential_node_id,credentials);
+                        res.send("<center><h1>You have successfully authenticated.</h1><h3>Please return to your node-red flow and close this tab/window, you will see that the 'access_token' is now filled in.</h3>"+additional_text+"</center>");
+                    } else {
+                        //something went wrong, show error.
+                        res.send(body);
+                    }
+
+                }
+            });
+
+            
+    });
+
+
+
+
+
+
 
     function MonzoNodeIn(config) {
         RED.nodes.createNode(this, config);
         this.requesttype = config.requesttype;
         this.monzoConfig = RED.nodes.getNode(config.monzocreds);
         const Monzo = require('monzo-js');
+
+
+        var node = this;
+
         if (!this.monzoConfig) {
             this.status({
                 fill: "red",
@@ -29,22 +204,37 @@ module.exports = function(RED) {
                 text: "ready"
             });
         }
-        var node = this;
+
+
+      
         node.on('input', function(msg) {
+
+            this.monzoConfig = RED.nodes.getNode(config.monzocreds);
+
+            var monzocredentials = RED.nodes.getCredentials(config.monzocreds);
+
+
             this.status({
                 fill: "yellow",
                 shape: "dot",
                 text: "requesting"
             });
+
+
             if (!this.monzoConfig) {
+
                 this.status({
                     fill: "red",
                     shape: "dot",
                     text: "no token"
                 });
                 node.error("you have not entered a token", msg);
+
             } else {
-                const monzo = new Monzo(this.monzoConfig.credentials.token);
+
+
+                const monzo = new Monzo(monzocredentials.token);
+
                 if (this.requesttype == "accounts") {
                     monzo.accounts.all().then(accounts => {
                         for (const [id, acc] of accounts) {
@@ -67,6 +257,7 @@ module.exports = function(RED) {
                         });
                     });
                 }
+
                 if (this.requesttype == "balances") {
                     monzo.accounts.all().then(accounts => {
                         for (const [id, acc] of accounts) {
@@ -89,6 +280,7 @@ module.exports = function(RED) {
                         });
                     });
                 }
+
                 if (this.requesttype == "pots") {
                     monzo.pots.all().then(pots => {
                         for (const [id, pot] of pots) {
@@ -114,6 +306,8 @@ module.exports = function(RED) {
                         });
                     });
                 }
+
+
             }
         });
     }
